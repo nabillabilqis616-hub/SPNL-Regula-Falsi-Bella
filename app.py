0270d6a7d75ef2692a695908b40cf1ebb0d8c0be
nabillabilqis616-hub <nabillabilqis616@gmail.com>
2025-12-07 21:41:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
   layout="centered"
 )
 
-st.title("Aplikasi ini digunakan untuk mencari akar persamaan non-linear tunggal menggunakan metode Regula Falsi, "
+st.title("Aplikasi ini digunakan untuk mencari akar persamaan non-linear tunggal menggunakan metode Regula Falsi "
 )
 
 st.sidebar.header("Input Parameter")
